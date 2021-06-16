8033f5de5178ea36e94594e94b914200103dd151
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-06-16 22:44:14 +0300

    var with fonction

--- Lesson1/app.js
+++ Lesson1/app.js
@@ -18,3 +18,32 @@ console.log('myNum : ' + myNum);
 
 var country = prompt('Enter your country : ');
 console.log('Your country is',country);
+
+
+var num1 = 42;
+
+if(num1 == 42) {
+    let num2 = 22;
+    console.log(num2);
+}
+// console.log(num2)   // num2 sadece if scope da tanımlı
+
+
+console.log(num3); // değer ataması sonradan yapıldığında 
+//var için "undifined",
+//let için "Uncaught ReferenceError: Cannot access 'num3' before initialization at app.js:32"
+var num3 = 3;
+// let num3 = 3;
+
+function funct1() {
+    var num4 = 6;
+    if (true){
+        console.log(num4)
+    }
+}
+
+// console.log(num4); 
+/* num4 var olarak tanımlandığı için ve var fonksiyon tabanlı
+ olduğu için dışarıda tanmlı olmaz*/
+
+ funct1()
