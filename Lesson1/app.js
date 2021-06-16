a4c3c7fdbfff9017a95d684dd59fd79fe51c21d8
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-06-16 21:42:39 +0300

    var_let_const

--- Lesson1/app.js
+++ Lesson1/app.js
@@ -1,8 +1,18 @@
-console.time();
-console.log('Hello world!');
-console.info('info')
-console.warn('warn');
-console.error('error');
+
+// console.log('Hello world!');
+// console.info('info')
+// console.warn('warn');
+// console.error('error');
 
 var name = 'Mark';
 console.log(name);
+
+
+const PI = 3.141521;
+const SCHOOL = 'Clarusway';
+console.log('PI : ' + PI);
+
+
+let myNum = 321;
+console.log('myNum : ' + myNum);
+
