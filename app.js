0f6471bfe8e9a2315a7a2bbdfa8759b013d60267
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-07-02 14:07:54 +0300

    functions

---
+++ app.js
@@ -0,0 +1,14 @@
+function hello() {
+    console.log("hello\n\n\n");
+}
+
+hello()
+
+
+function sum(a,b) {
+    return a+b; 
+}
+
+console.log("sum =",sum(3,4));
+console.log("typeof sum =>",typeof sum);
+console.log("typeof sum() =>",typeof sum());
