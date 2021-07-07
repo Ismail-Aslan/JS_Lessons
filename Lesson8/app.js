c21b16352eec6c294a321196f75e6c1c338604de
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-07-07 20:26:02 +0300

    .length

---
+++ Lesson8/app.js
@@ -0,0 +1,19 @@
+const cars = ['Audi','Opel', 'Fiat'];
+console.log(typeof cars);
+console.log(cars);
+
+const numbers1 = new Array(2,10);
+const numbers2 = new Array(10); // uzunluğu 10 olan boş bir array verir
+console.log(numbers1);
+console.log(numbers2);
+
+
+const cars2 = ['Audi','Opel', 'Fiat'];
+cars2[2] = 'BMW'
+console.log(cars2);
+
+
+// .length property --> uzunluğunu verir
+
+console.log(cars2.length);
+
