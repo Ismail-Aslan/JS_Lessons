d19074377348061716884a4d71fc7bd275fefae9
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-07-29 22:55:05 +0300

    quantity & remove button

--- LESSON11/checkout.js
+++ LESSON11/checkout.js
@@ -4,5 +4,22 @@ const urlParams = new URLSearchParams(queryString);
 console.log(urlParams);
 const firstname = urlParams.get("firstname");
 const lastname = urlParams.get("lastname");
-alert("Welcome " + firstname + " " + lastname);
+//alert("Welcome " + firstname + " " + lastname);
 
+const updateProductQuantity = (quantityInput) => {
+    alert(quantityInput.value);
+}
+document.querySelectorAll(".product-quantity input").forEach(function(input){
+    input.addEventListener("change", function(){
+        updateProductQuantity(this);
+    });
+});
+const removeProduct = (removeButton) => {
+    let productRow = removeButton.parentElement.parentElement;
+    productRow.remove();
+}
+document.querySelectorAll(".product-removal button").forEach(function(button){
+    button.addEventListener("click", function(){
+        removeProduct(this);
+    });
+});
