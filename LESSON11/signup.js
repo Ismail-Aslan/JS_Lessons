f8b1acf5010d9063ecd480c17fabf3cd76c241de
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-07-29 20:43:35 +0300

    2

--- LESSON11/signup.js
+++ LESSON11/signup.js
@@ -1,5 +1,12 @@
 const registrationform = document.getElementById("registration_form");
-registrationform.addEventListener("submit",function () {
-   window.location.href = "checkout.html?firsname=" + document.getElementsByName("first_name")[0].value + "&lastname=" + document.getElementsByName("last_name")[0].value;
-    
+registrationform.addEventListener("submit",function (e) {
+    e.preventDefault();
+
+   //window.location.href = "checkout.html?firsname=" + document.getElementsByName("first_name")[0].value + "&lastname=" + document.getElementsByName("last_name")[0].value;
+
+   alert(document.getElementsByName("first_name")[0].value + " " + document.getElementsByName("last_name")[0].value);
+   
 });
+
+
+const lastNameInput = document.getElementsByName("last_name")[0];
