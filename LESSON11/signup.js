7c72e79ff68cc3a3dcf3b5f052aef3c3b3872823
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-07-29 21:50:04 +0300

    regex used with email

--- LESSON11/signup.js
+++ LESSON11/signup.js
@@ -2,12 +2,53 @@ const registrationform = document.getElementById("registration_form");
 registrationform.addEventListener("submit",function (e) {
     e.preventDefault();
 
-   //window.location.href = "checkout.html?firsname=" + document.getElementsByName("first_name")[0].value + "&lastname=" + document.getElementsByName("last_name")[0].value;
+   window.location.href = "checkout.html?firsname=" + document.getElementsByName("first_name")[0].value + "&lastname=" + document.getElementsByName("last_name")[0].value;
 
-   alert(document.getElementsByName("first_name")[0].value + " " + document.getElementsByName("last_name")[0].value);
+   //alert(document.getElementsByName("first_name")[0].value + " " + document.getElementsByName("last_name")[0].value);
    
 });
 
 
 const lastNameInput = document.getElementsByName("last_name")[0];
-const remainCharSpan = document.getElementById("charCount")
+const remainCharSpan = document.getElementById("charCount");
+
+const showRemainChar = () => {
+    remainCharSpan.innerText = lastNameInput.value.length + "/40";
+    if (lastNameInput.value.length > 0) {
+        remainCharSpan.style.visibility = "visible";
+    } else {
+        remainCharSpan.style.visibility = "hidden";
+    }
+};
+lastNameInput.addEventListener("keyup",showRemainChar);
+
+
+let birthday = document.getElementsByName("birthday")[0];
+
+birthday.addEventListener("change", () => {
+    alert(new Date(birthday.value).toLocaleDateString() + " date is selected!");
+    
+});
+
+let email = document.getElementsByName("email")[0];
+
+const validateEmail = () => {
+    
+    let errorSpan = document.querySelector("#errorSpan");
+
+    const regexPattern = /\S+\@+\S+\.+\S/;
+    if (!regexPattern.test(email.value)) {
+        errorSpan.innerHTML = "Invalid email address!";
+        errorSpan.style.color = "red";
+    } else {
+        errorSpan.innerHTML = "Valid email address!";
+        errorSpan.style.color = "green";
+    }
+    if (email.value.length > 0) {
+        errorSpan.style.visibility = "visible";
+    } else {
+        errorSpan.style.visibility = "hidden";
+    }
+}
+
+email.addEventListener("keyup",validateEmail);
