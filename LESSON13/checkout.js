895f2216c7795a201f8d06c0a3363e03a376f023
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-08-12 21:16:03 +0300

    add onSubmit fnc to form

--- LESSON13/checkout.js
+++ LESSON13/checkout.js
@@ -1,16 +1,8 @@
-// const queryString = window.location.search;
-// console.log(queryString);
-// const urlParams = new URLSearchParams(queryString);
-// console.log(urlParams);
-// const firstname = urlParams.get("firstname");
-// const lastname = urlParams.get("lastname");
-// alert("welcome " + firstname + " " + lastname);
-
 
 
 
 function Product(name, price, quantity) {
-    console.log("product");
+    console.log("product's this");
     console.log(this);
     this.name = name;
     this.price = price;
@@ -20,12 +12,32 @@ console.log(this);
 
 
 function Display() {
-    console.log(display);
+    console.log("display's this");
     console.log(this);
-    
+    this.name = document.getElementById("name");
+    this.price = document.getElementById("price");
+    this.quantity = document.getElementById("quantity");
 }
 
 
+document.getElementById('customer-form').addEventListener("submit",function (event) {
+    console.log("....event....");
+    console.log(event);
+    console.log("form's this");
+    console.log(this);
+    event.preventDefault();
+
+
+
+    const name = this.getElementById("name");
+    const price = this.getElementById("price");
+    const quantity = this.getElementById("quantity");
+    const product = new Product(name.value,price.value,quantity.value);
+    const display = new Display();
+    console.log(display);
+    console.log(product);
+
+})
 
 
 
@@ -53,6 +65,13 @@ function Display() {
 
 
 
+// const queryString = window.location.search;
+// console.log(queryString);
+// const urlParams = new URLSearchParams(queryString);
+// console.log(urlParams);
+// const firstname = urlParams.get("firstname");
+// const lastname = urlParams.get("lastname");
+// alert("welcome " + firstname + " " + lastname);
 
 
 const updateProductQuantity = (input) => {
