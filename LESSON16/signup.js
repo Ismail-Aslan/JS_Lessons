a34bee62bae920c87af05dff9193f8e620e2bb53
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-08-30 22:04:16 +0300

    first

---
+++ LESSON16/signup.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    setTimeout(fillFormInput, 1000)
+});
+
+const fillFormInput = () => {
+    let email = document.getElementById("email").value = "eve.holt@reqres.in";
+    let password = document.getElementById("password").value = "pistol";
+    localStorage.setItem("email", email);
+}
