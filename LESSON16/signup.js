628a6004b858f08a255084a9c1cc46cbc398fad0
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-08-30 22:19:25 +0300

    create json object as bodyDate

--- LESSON16/signup.js
+++ LESSON16/signup.js
@@ -11,10 +11,15 @@ const fillFormInput = () => {
     let password = document.getElementById("password").value = "pistol";
     localStorage.setItem("email", email);
 
-    localStorage.setItem("email",email)
+    
 }
 const apiRegister = () => {
     let email = localStorage.getItem("email");
     let password = localStorage.getItem("password").value;
 
+    const bodyDate = {
+        email: email,
+        password: password
+    }
+    
 }
