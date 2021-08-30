caa468787b6520f2cee5741c73eb946de9211502
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-08-30 22:15:02 +0300

    create apiRegister func

--- LESSON16/signup.js
+++ LESSON16/signup.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
     setTimeout(fillFormInput, 1000)
-});
+}); // aşağıdaki yorum satırlarında yazan işlemin aynısı
+// window.onload = function(){
+//     setTimeout(fillFormInput, 1000)
+// };
+
 
 const fillFormInput = () => {
     let email = document.getElementById("email").value = "eve.holt@reqres.in";
     let password = document.getElementById("password").value = "pistol";
     localStorage.setItem("email", email);
+
+    localStorage.setItem("email",email)
+}
+const apiRegister = () => {
+    let email = localStorage.getItem("email");
+    let password = localStorage.getItem("password").value;
+
 }
